6f58092be235ab646958511d0658656d864679ea
thomgrd <128264872+thomgrd@users.noreply.github.com>
2023-03-31 15:28:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Sélectionnez le bouton et la div pour le pop-up
+const button = document.querySelector('.header-button');
+const popup = document.querySelector('#header-popup');
+
+// Ajoutez un événement click pour afficher le pop-up
+button.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
+
+// Ajoutez un événement click pour fermer le pop-up
+popup.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
+
+
+// Sélectionnez la croix
+const close = document.querySelector('.close');
+
+// Ajoutez un événement click pour fermer le pop-up
+close.addEventListener('click', () => {
+  popup.style.display = 'none';
+});
